ea24573f69e9bc5b906fdbc64823eb7aab601295
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-29 21:14:28 +0400

    FindIndexOfTheFirstOccurrenceInString

---
+++ src/main/java/FindIndexOfTheFirstOccurrenceInString.java
@@ -0,0 +1,15 @@
+public class FindIndexOfTheFirstOccurrenceInString {
+
+    public static void main(String[] args) {
+
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+
+        return haystack.indexOf(needle);
+    }
+
+}
