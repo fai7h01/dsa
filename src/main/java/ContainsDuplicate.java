447b7c8d7ac799a97b73a6f568b95aa498b1ff05
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-29 21:11:14 +0400

    ContainsDuplicate

---
+++ src/main/java/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+
+        int[] nums = {10,7,2,1};
+        System.out.println(containsDuplicate(nums));
+
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int each : nums) {
+            if (!set.add(each)) return true;
+        }
+        return false;
+    }
+
+
+}
