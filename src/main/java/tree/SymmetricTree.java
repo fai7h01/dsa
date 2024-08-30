c71f2ec81eafd8fb1eea6266200a052243be86f2
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-30 23:18:59 +0400

    isSymmetricTree

---
+++ src/main/java/tree/SymmetricTree.java
@@ -0,0 +1,20 @@
+package tree;
+
+public class SymmetricTree {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isSymmetric(TreeNode root){
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) return true;
+        if (node1 == null || node2 == null) return false;
+        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+    }
+
+}
