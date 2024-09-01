5849332723e72c0a1c4d0b52a883c6e579987b41
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-01 04:22:46 +0400

    HowManyNumsAreSmallerThanCurrentNumInArray

---
+++ src/main/java/HowManyNumsAreSmallerThanCurrentNumInArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class HowManyNumsAreSmallerThanCurrentNumInArray {
+
+    public static void main(String[] args) {
+        int[] nums = {8, 1, 2, 2, 3};
+        // ans: [4,0,1,1,3]
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(nums)));
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] copy = nums.clone();
+        Arrays.sort(copy);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < copy.length; i++) {
+            map.putIfAbsent(copy[i],i);
+        }
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = map.get(nums[i]);
+        }
+        return res;
+    }
+
+}
